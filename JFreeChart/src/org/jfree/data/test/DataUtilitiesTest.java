518df832c5ec0eaf5f51d10cced648b5d539be3b
FindlayMB <findlay.brown@ucalgary.ca>
2024-01-30 17:58:15 -0700

    Assignment 2 setup
    Unzipped jar files.
    Created a java project with jar files included in build path.

---
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -0,0 +1,15 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.junit.Test;
+
+public class DataUtilitiesTest extends DataUtilities {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
